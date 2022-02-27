18b1bec94a6f7b64addf8937c1128acb24f9ea94
zenx_lt <zenozhardstudio@gmail.com>
2022-02-27 19:37:30 +0700

    Original_Score
    nothings added

---
+++ Exercise4_Kittipon_S.py
@@ -0,0 +1,10 @@
+FE = 100
+GB = 98
+ICS = 94
+CP = 100
+
+print("                --- Your Score ---")
+print("Foundation English               :",FE," Points")
+print("General Business                 :",GB," Points")
+print("Introduction to Computer Systems :",ICS,"Points")
+print("Computer Programming             :",CP," Points")
